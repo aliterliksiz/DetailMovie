7009351b5ee40d170f4cf5bae5dfa7b50bb85204
aliterliksiz <aliterliksiz46@gmail.com>
2023-03-26 15:39:49 +0300

    new features added

--- DetailMovie/ViewController.swift
+++ DetailMovie/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var movieName: UILabel!
+    @IBOutlet weak var synopsis: UILabel!
+    @IBOutlet weak var synopsisTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        synopsis.font = UIFont(name:"HelveticaNeue-Bold", size: 16.0)
+        movieName.font = UIFont(name:"HelveticaNeue-Bold", size: 16.0)
     }
 
 
