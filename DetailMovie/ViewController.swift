eed21465df6280f12bb4fce101469379e2ff3752
aliterliksiz <aliterliksiz46@gmail.com>
2023-03-26 14:27:27 +0300

    Initial Commit

---
+++ DetailMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetailMovie
+//
+//  Created by Ali Terliksiz on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
